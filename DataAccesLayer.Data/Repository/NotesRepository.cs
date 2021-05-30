707dbe94074a4d8ae0d27c27a20160038463ac3b
onurinc <onur.i@hotmail.nl>
2021-05-30 06:32:09 +0200

    Implemented NotesRepository with the needed functions.

---
+++ DataAccesLayer.Data/Repository/NotesRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccesLayer.Data.Context;
+using DataAccesLayer.Data.Data_Transfer_Object;
+using DataAccesLayer.Data.InterfaceContext;
+
+namespace DataAccesLayer.Data.Repository
+{
+    class NotesRepository
+    {
+        private INotesContext context;
+        public NotesRepository(INotesContext context)
+        {
+            this.context = context;
+        }
+
+        public NotesRepository()
+        {
+            this.context = new NotesContext();
+        }
+
+        public List<NotesDTO> GetAllNotes()
+        {
+            return context.GetAllNotes().ToList();
+        }
+
+        public NotesDTO GetNote(int id)
+        {
+            return context.GetNote(id);
+        }
+
+        public void AddProject(NotesDTO note)
+        {
+            context.AddNote(note);
+        }
+
+        public void EditProject(NotesDTO note)
+        {
+            context.EditNote(note);
+        }
+
+        public void DeleteProject(int id)
+        {
+            context.DeleteNote(id);
+        }
+    }
+}
