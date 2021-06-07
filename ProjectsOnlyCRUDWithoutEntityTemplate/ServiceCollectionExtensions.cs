17804be9ec5fb35c9b8fbfb6dd4676a104b4d62d
onurinc <onur.i@hotmail.nl>
2021-06-07 10:45:22 +0200

    Moved ServiceCollectionExtensions into the Presentation Layer.

---
+++ ProjectsOnlyCRUDWithoutEntityTemplate/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using DataAccesLayer.Data;
+using DataAccesLayer.Data.Context;
+using DataAccesLayer.Data.InterfaceContext;
+using DataAccesLayer.Data.InterfaceRepository;
+using LogicLayer.Container;
+using LogicLayer.InterfaceContainer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DataAccesLayer.Data.Repository;
+
+namespace ProjectsOnlyCRUDWithoutEntityTemplate
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterDataServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IProjectsRepository, ProjectsRepository>();
+            services.AddScoped<IProjectsContext, ProjectsContext>();
+            services.AddScoped<IProjectContainer, ProjectContainer>();
+
+            services.AddScoped<INotesRepository, NotesRepository>();
+            services.AddScoped<INotesContext, NotesContext>();
+            services.AddScoped<INotesContainer, NotesContainer>();
+
+            return services;
+        }
+    }
+}
