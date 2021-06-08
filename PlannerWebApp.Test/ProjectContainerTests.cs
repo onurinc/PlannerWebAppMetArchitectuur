a17344e19d92eb30ccbac6b18b80855d3c1e03af
onurinc <onur.i@hotmail.nl>
2021-06-08 09:40:23 +0200

    Added GetProject Unit Test for ProjectContainer. Removed LogicLayer.Test since I don't use it anymore.

---
+++ PlannerWebApp.Test/ProjectContainerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DataAccesLayer.Data;
+using DataAccesLayer.Data.InterfaceRepository;
+using LogicLayer.Container;
+using Moq;
+using Xunit;
+
+namespace PlannerWebApp.Test
+{
+    public class ProjectContainerTests
+    {
+        private readonly ProjectContainer _projectContainer;
+        private readonly Mock<IProjectsRepository> _projectRepoMock = new Mock<IProjectsRepository>();
+
+        public ProjectContainerTests()
+        {
+            _projectContainer = new ProjectContainer(_projectRepoMock.Object);
+        }
+
+
+        [Fact]
+        public void GetProject_ShouldReturnProject_WhenProjectExists()
+        {
+            // Arrange
+            var id = 1;
+            var projectsDTO = new ProjectsDTO
+            {
+                ProjectId = 1,
+                ProjectName = "ProjectOne"
+            };
+
+            _projectRepoMock.Setup(x => x.GetProject(id)).Returns(projectsDTO);
+
+
+            // Act
+            var project = _projectContainer.GetProjectById(id);
+
+            // Assert
+            Assert.Equal(id, project.ProjectId);
+
+        }
+    }
+}
